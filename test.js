e497633c054effc5fee17720805b4341c22887cc
Evandro Leopoldino Gonalves <evandrolgoncalves@gmail.com>
2017-02-05 21:53:02 -0200

    remove tabs

--- test.js
+++ test.js
@@ -3,17 +3,17 @@ var findByWords = require('./find-by-words');
 
 describe('find by words', function() {
 
-	var verifyOutput = function(list, word, expected) {
-		var output = findByWords(list, word);
-		assert(expected.every((v, i) => v == output[i]));
-	};
+  var verifyOutput = function(list, word, expected) {
+    var output = findByWords(list, word);
+    assert(expected.every((v, i) => v == output[i]));
+  };
 
-	it('should return an empty array', function() {
-		verifyOutput(['banana', 'orange', 'apple'], 'gra', []);
-	});
+  it('should return an empty array', function() {
+    verifyOutput(['banana', 'orange', 'apple'], 'gra', []);
+  });
 
-	it('should return an array with two items', function() {
-		verifyOutput(['python', 'ruby', 'java', 'javascript', 'lua'],
-								'java', ['java', 'javascript']);
-	});
+  it('should return an array with two items', function() {
+    verifyOutput(['python', 'ruby', 'java', 'javascript', 'lua'],
+        'java', ['java', 'javascript']);
+  });
 });
