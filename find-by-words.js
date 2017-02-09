818a6b40944d7ffc57c054d469c11de38d62d931
Evandro Leopoldino Gonalves <evandrolgoncalves@gmail.com>
2017-02-09 11:24:04 -0200

    add support to different modules

--- find-by-words.js
+++ find-by-words.js
@@ -1,35 +1,48 @@
-var findByWords = function(a, word) {
-  var size = word.length;
-  var first = 0;
-  var last = a.length - 1;
-  var found = false;
-  var output = [];
+/*
+ * find-by-words
+ * author: Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
+ * https://github.com/evandrolg
+ * License: MIT
+*/
 
-  while(first <= last && !found) {
-    var middle = Math.ceil((first + last) / 2);
-    var wordPart = a[middle].slice(0, size);
+(function (name, context, definition) {
+  if (typeof module != 'undefined' && module.exports) module.exports = definition();
+  else if (typeof define == 'function' && define.amd) define(definition)
+  else context[name] = definition()
+})('findByWords', this, function (name, context) {
+  var findByWords = function(a, word) {
+    var size = word.length;
+    var first = 0;
+    var last = a.length - 1;
+    var found = false;
+    var output = [];
 
-    if (wordPart == word) {
-      found = true;
-      output.push(a[middle]);
-      var _middle = middle;
+    while(first <= last && !found) {
+      var middle = Math.ceil((first + last) / 2);
+      var wordPart = a[middle].slice(0, size);
 
-      while (a[--_middle].slice(0, size) == word) {
-        output.push(a[_middle]); 
+      if (wordPart == word) {
+        found = true;
+        output.push(a[middle]);
+        var _middle = middle;
+
+        while (a[--_middle].slice(0, size) == word) {
+          output.push(a[_middle]);
+        }
+
+        while (a[++middle].slice(0, size) == word) {
+          output.push(a[middle]);
+        }
+
+      } else if (wordPart > word) {
+        last = middle - 1;
+      } else {
+        first = middle + 1;
       }
-
-      while (a[++middle].slice(0, size) == word) {
-        output.push(a[middle]); 
-      }
-
-    } else if (wordPart > word) {
-      last = middle - 1;
-    } else {
-      first = middle + 1;
     }
-  }
 
-  return output;
-};
+    return output;
+  };
 
-module.exports = findByWords;
+  return findByWords;
+});
